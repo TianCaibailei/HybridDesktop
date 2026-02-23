46595c6e6de61290fd289e398dd1d5d1d3bf15bf
devil <yang.lj@yungku.com>
2026-02-23 11:58:38 +0800

    feat: implement high-frequency data channels
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-opencode)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ HybridApp.Core/Channels/FloatDataChannel.cs
@@ -0,0 +1,38 @@
+using Microsoft.Web.WebView2.Core;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace HybridApp.Core.Channels
+{
+    public class FloatDataChannel
+    {
+        private readonly CoreWebView2 _webView;
+        private readonly string _channelName;
+        private readonly CoreWebView2SharedBuffer _sharedBuffer;
+
+        public FloatDataChannel(CoreWebView2 webView, string channelName, int maxElements)
+        {
+            _webView = webView;
+            _channelName = channelName;
+            
+            ulong size = (ulong)(maxElements * sizeof(float));
+            _sharedBuffer = _webView.Environment.CreateSharedBuffer(size);
+            
+            // Post shared buffer to script
+            _webView.PostSharedBufferToScript(_sharedBuffer, CoreWebView2SharedBufferAccess.ReadOnly, channelName);
+        }
+
+        public void Push(float[] data)
+        {
+            using (var stream = _sharedBuffer.OpenStream())
+            {
+                byte[] byteData = new byte[data.Length * sizeof(float)];
+                Buffer.BlockCopy(data, 0, byteData, 0, byteData.Length);
+                stream.Write(byteData, 0, byteData.Length);
+            }
+            
+            _webView.PostWebMessageAsString($"SHARED_MEM_READY:{_channelName}:{data.Length}");
+        }
+    }
+}
---
+++ HybridApp.Core/Channels/ImageStreamManager.cs
@@ -0,0 +1,33 @@
+using Microsoft.Web.WebView2.Core;
+using System;
+using System.IO;
+
+namespace HybridApp.Core.Channels
+{
+    public class ImageStreamManager
+    {
+        public Func<string, byte[]?>? OnImageRequested { get; set; }
+
+        public void Attach(CoreWebView2 webView)
+        {
+            webView.AddWebResourceRequestedFilter("http://hybrid.vision/*", CoreWebView2WebResourceContext.Image);
+            webView.WebResourceRequested += (sender, args) =>
+            {
+                var uri = new Uri(args.Request.Uri);
+                var channelName = uri.AbsolutePath.TrimStart('/');
+                
+                var imageData = OnImageRequested?.Invoke(channelName);
+                
+                if (imageData != null)
+                {
+                    var stream = new MemoryStream(imageData);
+                    args.Response = webView.Environment.CreateWebResourceResponse(stream, 200, "OK", "Content-Type: image/jpeg");
+                }
+                else
+                {
+                    args.Response = webView.Environment.CreateWebResourceResponse(null, 404, "Not Found", "");
+                }
+            };
+        }
+    }
+}
