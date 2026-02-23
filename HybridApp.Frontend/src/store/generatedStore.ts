8f0b90337751a1833fe80a1d7f80aed221d3e51c
devil <yang.lj@yungku.com>
2026-02-23 12:26:35 +0800

    feat: add frontend hooks and components for high-speed channels

--- HybridApp.Frontend/src/store/generatedStore.ts
+++ HybridApp.Frontend/src/store/generatedStore.ts
@@ -1,1 +1,4 @@
-import { create } from 'zustand'; export const useAppStore = create(() => ({}));
+import { create } from 'zustand';
+// This is a placeholder store that will be overwritten by the generator.
+// It needs to exist for compilation purposes.
+export const useAppStore = create(() => ({}));
